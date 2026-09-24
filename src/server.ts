f5df49e4f6acabd6adb945a020a92d441205735d
Hugo <levssshe>
2026-09-24 18:11:41 +0900

    feat: integration of environmental varible

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,2 @@
-console.log("EXECUTING ");
-
-import moment from "moment";
-
-const currentDate = moment().format("YYYY-MM-DD HH:mm:ss");
-
-const person: string = "Hugo";
-const age: number = 20;
+import dotenv from "dotenv";
+dotenv.config();
